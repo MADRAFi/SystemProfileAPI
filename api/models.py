6500edf5638f1c7ed39a2ac171668b375c337176
MADRAFi <madrafi@gmail.com>
2021-11-14 17:24:13 +0100

    addig support for baselines in profiles

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql.elements import False_
 from sqlalchemy.sql.expression import null, text
 from sqlalchemy.sql.schema import ForeignKey, ForeignKeyConstraint
 from .database import Base
@@ -9,12 +10,16 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP, Integer
 class Profile(Base):
     __tablename__ = "profiles"
  
-    name = Column(String, primary_key=True, nullable=False, unique=True, index=True)
-    fqdn = Column(String, nullable=False)
+    fqdn = Column(String, primary_key=True, nullable=False, unique=True, index=True)
+    # fqdn = Column(String, nullable=False)
+    baseline_id = Column(Integer, ForeignKey('baselines.id'), nullable=False, index=True)
     ip = Column(String, nullable=False)
     netmask = Column(String, nullable=False)
     gateway = Column(String, nullable=False)
-    created = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('datetime()'))
+    default_pass = Column(String, nullable=False)
+    created = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("(datetime())"))
+
+    baselines = relationship("Baseline", back_populates="profiles")
 
 class Baseline(Base):
     __tablename__ = "baselines"
@@ -25,6 +30,7 @@ class Baseline(Base):
     url = Column(String, nullable=False)
 
     systemos = relationship("SystemOS", back_populates="baselines")
+    profiles = relationship("Profile", back_populates="baselines")
 
 class SystemOS(Base):
     __tablename__ = "systems"
